e885bd204d6cc4e7f64a18ef2a8490744b4eb903
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-12-11 14:30:26 +0900

    交差型

--- section-6/Understanding-TS-course-starting-project/src/app.ts
+++ section-6/Understanding-TS-course-starting-project/src/app.ts
@@ -1,1 +1,30 @@
-console.log('ここにコードを追加します。');
+/*
+ * 交差型 -> ２つの型を組み合わせる
+ */
+
+type Admin = {
+  name: string;
+  privileges: string[]; // 権限を追加していく
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+// interface ElevatedEmployee extends Admin, Employee {} //? ２つの型を組み合わせる
+
+type ElevatedEmployee = Admin & Employee; //? ２つの型を組み合わせる
+
+const e1: ElevatedEmployee = {
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+// union型は、同じものだけが組み合わせられる
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric; //? ２つの型を組み合わせる
