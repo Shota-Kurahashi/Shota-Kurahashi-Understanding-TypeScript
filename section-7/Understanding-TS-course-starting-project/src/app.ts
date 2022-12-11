70b2e4f7d83b26c97d1dd3c23cc79bf2f42a73e6
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-12-11 23:39:59 +0900

    generics 追加

--- section-7/Understanding-TS-course-starting-project/src/app.ts
+++ section-7/Understanding-TS-course-starting-project/src/app.ts
@@ -47,3 +47,63 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 extractAndConvert({ name: "Max" }, "name");
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) return;
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+
+const numberStorage = new DataStorage<number>();
+
+/* const objStorage = new DataStorage<object>();
+
+const obj = {
+  name: "Max",
+};
+objStorage.addItem(obj);
+
+objStorage.addItem({ name: "Manu" });
+// ...
+
+// このobjは参照が違うので削除されない
+objStorage.removeItem(obj);
+
+console.log(objStorage.getItems()); */
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  completeUntil: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = completeUntil;
+
+  return courseGoal as CourseGoal;
+}
+
+//* 読み取り専用
+const namess: Readonly<string[]> = ["Max", "Anna"];
+// namess.push("Manu");
