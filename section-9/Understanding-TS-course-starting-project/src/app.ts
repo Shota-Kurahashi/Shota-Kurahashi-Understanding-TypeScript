f2044fbd762d771deb36357b5a732c65d9f08086
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-12-14 16:04:50 +0900

    project listのクラスを作成するよ

--- section-9/Understanding-TS-course-starting-project/src/app.ts
+++ section-9/Understanding-TS-course-starting-project/src/app.ts
@@ -1,3 +1,65 @@
+/* Project Type */
+
+enum ProjectStatus {
+  Active,
+  Finished,
+}
+
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public manday: number,
+    public status: ProjectStatus
+  ) {}
+}
+/* project State Management */
+
+type Listener<T> = (items: T[]) => void;
+
+class State<T> {
+  protected listeners: Listener<T>[] = [];
+
+  addListener(listenerFn: Listener<T>) {
+    this.listeners.push(listenerFn);
+  }
+}
+
+class ProjectState extends State<Project> {
+  private projects: Project[] = [];
+
+  private static instance: ProjectState;
+
+  private constructor() {
+    super();
+  }
+
+  static getInstance(): ProjectState {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new ProjectState();
+    return this.instance;
+  }
+
+  addProject(title: string, description: string, manday: number) {
+    const newProject = new Project(
+      Math.random().toString(),
+      title,
+      description,
+      manday,
+      ProjectStatus.Active
+    );
+    this.projects.push(newProject);
+    for (const listenersFn of this.listeners) {
+      listenersFn(this.projects.slice());
+    }
+  }
+}
+
+const projectState = ProjectState.getInstance();
+
 /* validation */
 interface Validatable {
   value: string | number;
@@ -46,11 +108,7 @@ function validate(validatableInput: Validatable) {
 }
 
 /* AutoBind */
-function AutoBind(
-  target: any,
-  propertyName: string,
-  description: PropertyDescriptor
-) {
+function AutoBind(_: any, _2: string, description: PropertyDescriptor) {
   const propertyMethod = description.value;
 
   const adjDescriptor: PropertyDescriptor = {
@@ -63,30 +121,100 @@ function AutoBind(
   return adjDescriptor;
 }
 
-// ルストの中のプロジェクトを表すクラス
+/* Component Base Class */
+
+abstract class Components<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement;
+  hostElement: T;
+  element: U;
+
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    insertAtStart: boolean,
+    newElementId?: string
+  ) {
+    this.templateElement = document.getElementById(
+      templateId
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId)! as T;
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild! as U;
+
+    if (newElementId) {
+      this.element.id = newElementId;
+    }
+
+    this.attach(insertAtStart);
+  }
+
+  abstract configure(): void;
+  abstract renderContent(): void;
+
+  attach(insertAtStartBeginning: boolean) {
+    this.hostElement.insertAdjacentElement(
+      insertAtStartBeginning ? "afterbegin" : "beforeend",
+      this.element
+    );
+  }
+}
 
 // リストを表示するクラス
 
-class ProjectList {}
+class ProjectList extends Components<HTMLDivElement, HTMLElement> {
+  assignedProjects: Project[];
 
-class ProjectInput {
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
-  element: HTMLFormElement;
+  constructor(private type: "active" | "finished") {
+    super("project-list", "app", false, `${type}-projects`);
+    this.assignedProjects = [];
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {
+    projectState.addListener((projects: Project[]) => {
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === "active") {
+          return prj.status === ProjectStatus.Active;
+        }
+        return prj.status === ProjectStatus.Finished;
+      });
+      this.assignedProjects = relevantProjects;
+      this.renderProjects();
+    });
+  }
+
+  renderContent() {
+    const listId = `${this.type}-project-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type === "active" ? "実行中のプロジェクト" : "完了したプロジェクト";
+  }
+
+  private renderProjects() {
+    const listEl = document.getElementById(
+      `${this.type}-project-list`
+    )! as HTMLUListElement;
+
+    listEl.innerHTML = "";
+    for (const prjItem of this.assignedProjects) {
+      const listItem = document.createElement("li");
+      listItem.textContent = prjItem.title;
+      listEl.appendChild(listItem);
+    }
+  }
+}
+
+class ProjectInput extends Components<HTMLDivElement, HTMLFormElement> {
   titleInputElement: HTMLInputElement;
   descriptionInputElement: HTMLInputElement;
   mandayInputElement: HTMLInputElement;
 
   constructor() {
-    this.templateElement = document.querySelector("#project-input")!;
-    this.hostElement = document.querySelector("#app")!;
-    const importedNode = document.importNode(
-      this.templateElement.content,
-      true
-    );
-    this.element = importedNode.firstElementChild! as HTMLFormElement;
-    this.element.id = "user-input";
-
+    super("project-input", "app", true, "user-input");
     this.titleInputElement = this.element.querySelector(
       "#title"
     ) as HTMLInputElement;
@@ -100,8 +228,13 @@ class ProjectInput {
     )! as HTMLInputElement;
 
     this.configure();
-    this.attach();
   }
+
+  configure() {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+
+  renderContent() {}
 
   private clearInput() {
     this.titleInputElement.value = "";
@@ -151,17 +284,12 @@ class ProjectInput {
 
     if (Array.isArray(userInput)) {
       const [title, desc, manday] = userInput;
+      projectState.addProject(title, desc, manday);
       this.clearInput();
     }
-  }
-
-  private configure() {
-    this.element.addEventListener("submit", this.submitHandler);
-  }
-
-  private attach() {
-    this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList("active");
+const finishedPrjList = new ProjectList("finished");
