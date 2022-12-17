1a72d85c807a27d250eb94a4bee8b1629474a281
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-12-17 13:43:15 +0900

    section-12終わり

--- section-12/Understanding-TS-course-starting-project/src/app.ts
+++ section-12/Understanding-TS-course-starting-project/src/app.ts
@@ -1,6 +1,30 @@
-import { ProjectInput } from "./components/project-input";
-import { ProjectList } from "./components/project-list";
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+import { Product } from "./product.model";
+import { validate } from "class-validator";
 
-new ProjectInput();
-new ProjectList("active");
-new ProjectList("finished");
+const products = [
+  {
+    title: "本",
+    price: 100,
+  },
+  {
+    title: "ノート",
+    price: 200,
+  },
+];
+
+const newProd = new Product("", -5.99);
+console.log(newProd.getInformation());
+
+const loadedProduct = plainToClass(Product, products);
+console.log(loadedProduct);
+
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("入力情報にエラーがあります");
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
